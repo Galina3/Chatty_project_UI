cba0f77694a7b585b4ee7e1fc00718136c28692b
Kanivetsa <anna.kanivets.16@gmail.comgit config --global user.name Kanivetsagit config --global user.email  anna.kanivets.16@gmail.com>
2024-11-22 19:45:39 +0100

    ContactUsFormTest added

---
+++ src/main/java/randomGenerator/Main.java
@@ -0,0 +1,9 @@
+package randomGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(RandomDataUtil.getRandomValidEmail());
+        System.out.println(RandomDataUtil.getValidRandomPhone());
+
+        }
+}
---
+++ src/test/java/ContactUsFormTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+import pageObjects.ContactPage;
+import pageObjects.HomePage;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ContactUsFormTest extends UserRegistrationAndRemoval {
+    private final String username = "tester";
+    private final String validEmail = "tester5689@gmail.com";
+
+    @Test
+    public void submitContactUsFormWithValidDataTest() {
+        new HomePage(driver)
+                .openContactPage();
+        new ContactPage(driver)
+                .inputUsername(username)
+                .inputEmail(validEmail)
+                .inputMessage("Lorem hdhjdkdh")
+                .clickSendMessageButton();
+        assertTrue(new ContactPage(driver).isMessageSent());
+    }
+    @Test
+    public void submitContactUsFormWithEmptyMessageFieldTest() {
+        new HomePage(driver)
+                .openContactPage();
+        new ContactPage(driver)
+                .inputUsername(username)
+                .inputEmail(validEmail);
+        assertFalse(new ContactPage(driver).isMessageSent());
+    }
+    @Test
+    public void submitContactUsFormWithInvalidEmailTest() {
+        new HomePage(driver)
+                .openContactPage();
+        new ContactPage(driver)
+                .inputEmail("InValidEmail");
+        assertTrue(new ContactPage(driver).isErrorMessageInvalidEmailDisplayed());
+    }
+}
